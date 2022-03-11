a44d72946a5686dbbe2357e666dc4cebcb954332
Josh-Gotro <joshuagauthreaux@gmail.com>
2022-03-10 21:08:44 -0600

    okxeru

--- data.py
+++ data.py
@@ -2,5 +2,5 @@ general = ["kittens", "UwU", "puppies", "silly",
            "teatime", "baby+giggle", "sparkle+pony"]
 youtube = ["tinycakes", "my little pony",
            "kid sister and me", "24 hours of laugter"]
-inspiration = ["silly", "everything+is+going+to+be+ok",
+inspiration = ["silly", "island+life",
                "always+look+on+the+bright+side", "you+are+special", "someone+loves+you", "kindess", "barneysong", "i+will+always+love +you", "satin+sunsets+Liam+Aidan"]

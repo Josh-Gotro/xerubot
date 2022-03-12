9dee95047059b14fef52c4f4ccee0e0cf6316a12
Josh-Gotro <joshuagauthreaux@gmail.com>
2022-03-12 15:07:54 -0600

    expand badxeru word list

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ youtube = ["tinycakes", "my little pony",
            "kid sister and me", "24 hours of laugter"]
 inspiration = ["silly", "island+life",
                "always+look+on+the+bright+side", "you+are+special", "someone+loves+you", "kindess", "barneysong", "i+will+always+love +you", "satin+sunsets+Liam+Aidan"]
-bad = ["fake+news", "delete+facebook",
+bad = ["delete+facebook",
        "flat+earther", "conspiracy", "orlly", "enserio", "bitch+please", "wat", "troll+toll", "dumb+dumber", "buster+bluth"]
